08b0a79a7ff924fc828be1216166b2ae01d3e5a1
= <=>
2016-06-02 13:55:38 +0800

    Update Views frame and configuration.

---
+++ Application/Views/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //'DEFAULT_V_LAYER' => 'Template', //自定义视图结构 默认是View文件夹下
+   // 'TMPL_TEMPLATE_SUFFIX' => '.dragon', //自定义模板文件后缀
+   // 'TMPL_FILE_DEPR' => '_',//链接符
+   // 'VIEW_PATH' => './Theme/Views/', //自定义视图文件目录
+);
