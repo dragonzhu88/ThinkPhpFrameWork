74369f9e798718c042927832ee2694e1ea11de34
= <=>
2016-06-02 10:35:26 +0800

    Update user configure files.

---
+++ Application/User/Conf/upload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dragon
+ * Date: 16-6-1
+ * Time: 下午8:35
+ */
+
+return array(
+    //'配置项'=>'配置值'
+    'UPLOAD_NAME'    => 'dragon_upload',
+
+);
---
+++ Application/User/Conf/user.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dragon
+ * Date: 16-6-1
+ * Time: 下午8:33
+ */
+
+return array(
+    //'配置项'=>'配置值'
+ 'USER_NAME'    => 'dragon',
+
+);
